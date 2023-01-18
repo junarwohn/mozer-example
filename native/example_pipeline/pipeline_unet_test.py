886d017009ebe2477a758f8c837c2c0b79be0a06
Joonho Wohn <givenarc@gmail.com>
2023-01-18 10:29:16 +0000

    add long-skip-connection(unet) example

--- native/example_pipeline/pipeline_unet_test.py
+++ native/example_pipeline/pipeline_unet_test.py
@@ -35,57 +35,24 @@ from PIL import Image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import time
 from tvm.contrib import graph_executor, pipeline_executor, pipeline_executor_build
+import cv2
 
 parser = ArgumentParser()
-parser.add_argument('--partition', '-p', type=int, default=25)
+parser.add_argument('--partition', '-p', type=int, default=7)
 parser.add_argument('--batch_size', '-b', type=int, default=1)
 args = parser.parse_args()
 
 ################################################
-# import resnet50
-# if True:
-if False:
-    weights_url = "".join(
-        [
-            " https://storage.googleapis.com/tensorflow/keras-applications/",
-            "resnet/resnet50_weights_tf_dim_ordering_tf_kernels.h5",
-        ]
-    )
-    weights_file = "resnet50_keras_new.h5"
 
-
-    weights_path = download_testdata(weights_url, weights_file, module="keras")
-    model_keras = tf.keras.applications.resnet.ResNet50(
-        include_top=True, weights=None, input_shape=(224, 224, 3), classes=1000
-    )
-    model_keras.load_weights(weights_path)
-################################################
-
-
-################################################
-# import resnet152
-if True:
-# if False:
-    weights_url = "".join(
-        [
-            " https://storage.googleapis.com/tensorflow/keras-applications/",
-            "resnet/resnet152_weights_tf_dim_ordering_tf_kernels.h5",
-        ]
-    )
-    weights_file = "resnet152_keras_new.h5"
-
-
-    weights_path = download_testdata(weights_url, weights_file, module="keras")
-    model_keras = tf.keras.applications.resnet.ResNet152(
-        include_top=True, weights=None, input_shape=(224, 224, 3), classes=1000
-    )
-    model_keras.load_weights(weights_path)
+# import unet
+model_keras = tf.keras.models.load_model("/home/jd/workspace/data/UNet.h5")
+tf.keras.utils.plot_model(model_keras, show_shapes=True, show_layer_names=True, to_file='{}.png'.format(model_keras.name))
 
 ################################################
 
 img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
 img_path = download_testdata(img_url, "cat.png", module="data")
-img = Image.open(img_path).resize((224, 224))
+img = Image.open(img_path).resize((256, 256))
 # data = np.array(img)[np.newaxis, :].astype("float32")
 data = np.array(img).astype("float32")
 data = np.array([data for _ in range(args.batch_size)])
@@ -93,33 +60,57 @@ data = preprocess_input(data).transpose([0, 3, 1, 2])
 
 ################################################
 
-synset_url = "".join(
-    [
-        "https://gist.githubusercontent.com/zhreshold/",
-        "4d0b62f3d01426887599d4f7ede23ee5/raw/",
-        "596b27d23537e5a1b5751d2b0481ef172f58b539/",
-        "imagenet1000_clsid_to_human.txt",
-    ]
-)
-synset_name = "imagenet1000_clsid_to_human.txt"
-synset_path = download_testdata(synset_url, synset_name, module="data")
-with open(synset_path) as f:
-    synset = eval(f.read())
-
-################################################
-
 shape_dict = {"input_1": data.shape}
 mod, params = relay.frontend.from_keras(model_keras, shape_dict)
 
+target = "cuda"
+dev = tvm.device("cuda", 0)
 
+with tvm.transform.PassContext(opt_level=4):
+    lib = relay.build(mod, target, params=params)
+total_outs = []
+iter = 100
+model = graph_executor.GraphModule(lib["default"](dev))
+for _ in range(iter):
+    model.set_input('input_1', data)
+    model.run()
+    out = model.get_output(0).numpy()
+    total_outs.append(1)
+
+iter = 100
+stime = time.time()
+model = graph_executor.GraphModule(lib["default"](dev))
+for _ in range(iter):
+    model.set_input('input_1', data)
+    model.run()
+    out = model.get_output(0).numpy()
+    total_outs.append(1)
+    out = np.squeeze(out.transpose([0,2,3,1]))
+    total_outs.append(1)
+
+etime = time.time()
+
+print(f"single time : {etime-stime}")
+
+original_out = out
+# original_out = []
+# exit()
+del model
+
+# visualized
+# print(out.shape)
+# cv2.imshow("as", original_out)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 relay_slicer = TVMSlicer()
-# print(relay_slicer.get_node_count(mod, is_op('add')(wildcard(), wildcard())))
-# exit()
-split_config = [{"op_name": "add", "op_index": args.partition}]
+print(relay_slicer.get_node_count(mod, is_op('nn.leaky_relu')(wildcard())))
+
+split_config = [{"op_name": "nn.leaky_relu", "op_index": args.partition}]
 
 subgraphs, input_name_hints, output_name_hints = relay_slicer.slice_relay_graph(mod['main'], split_config, params)
-
+# print(len(subgraphs))
+# exit()
 for idx, graph in enumerate(subgraphs):
     ann = run_opt_pass(graph, transform.ToGraphNormalForm())
     subgraphs[idx] =  tvm.IRModule.from_expr(ann)
@@ -168,28 +159,62 @@ pipe_config[mod1]["output"][0].connect(pipe_config["output"][0])
 with tvm.transform.PassContext(opt_level=4):
     pipeline_mod_factory = pipeline_executor_build.build(pipe_config)
 
+
+
 # exit()
 # directory_path = tvm.contrib.utils.tempdir().temp_dir
 # os.makedirs(directory_path, exist_ok=True)
 # config_file_name = pipeline_mod_factory.export_library(directory_path)
 # pipeline_module = pipeline_executor.PipelineModule.load_library(config_file_name)
 pipeline_module = pipeline_executor.PipelineModule(pipeline_mod_factory)
-data = tvm.nd.array(data)
+# idata = tvm.nd.array(data)
+iter = 1
+# iter = 1
+total_outs = []
+after_burn = 0
+for i in range(iter):
+    idata = tvm.nd.array(data)
+    pipeline_module.set_input("input_1", idata)
+    # time.sleep(5)
+
+    pipeline_module.run()
+    # time.sleep(5)
+    # outputs = pipeline_module.get_output(synchronize=False)
+
+    outputs = pipeline_module.get_output(synchronize=True)
+    # time.sleep(5)
+    # if i - len(total_outs) + 1 > 2 and len(outputs) == 0:
+    #     outputs = pipeline_module.get_output(synchronize=True)
+
+    if outputs:
+        for out in outputs:
+            partition_out = np.squeeze(out.numpy().transpose([0,2,3,1]))
+            total_outs.append(1)
+
+while len(total_outs) != iter:
+    if True:
+        outputs = pipeline_module.get_output(synchronize=True)
+
+    if outputs:
+        for out in outputs:
+            partition_out = out.numpy()
+            partition_out = np.squeeze(partition_out.transpose([0,2,3,1]))
+            total_outs.append(1)
+
+
+total_outs = []
 iter = 100
 # iter = 1
-total_outs = []
-after_burn = 0
+stime = time.time()
 for i in range(iter):
-    pipeline_module.set_input("input_1", data)
+    pipeline_module.set_input("input_1", idata)
     pipeline_module.run()
     outputs = pipeline_module.get_output(synchronize=False)
-    # if i - len(total_outs) + 1 > 5 and len(outputs) == 0:
-    #     outputs = pipeline_module.get_output(synchronize=True)
 
     if outputs:
         for out in outputs:
-            partition_out = out.numpy()[0]
-            top1_keras = np.argmax(partition_out)
+            partition_out = out.numpy()
+            partition_out = np.squeeze(partition_out.transpose([0,2,3,1]))
             total_outs.append(1)
 
 
@@ -199,97 +224,80 @@ while len(total_outs) != iter:
 
     if outputs:
         for out in outputs:
-            partition_out = out.numpy()[0]
-            top1_keras = np.argmax(partition_out)
+            partition_out = out.numpy()
+            partition_out = np.squeeze(partition_out.transpose([0,2,3,1]))
             total_outs.append(1)
 
-total_outs = []
-iter = 1000
-# iter = 100
-# iter = 1
-after_burn = 0
-now = time.time()
-for i in range(iter):
-    print(1)
-    pipeline_module.set_input("input_1", data)
-    pipeline_module.run()
-    outputs = pipeline_module.get_output(synchronize=False)
+etime = time.time()
+print("end pipeline")
+print(f"pipeline time : {etime-stime}")
 
-    # if i - len(total_outs) + 1 > 5 and len(outputs) == 0:
-    #     outputs = pipeline_module.get_output(synchronize=True)
 
-    if outputs:
-        for out in outputs:
-            partition_out = out.numpy()[0]
-            top1_keras = np.argmax(partition_out)
-            total_outs.append(1)
 
-while len(total_outs) != iter:
-    if True:
-        outputs = pipeline_module.get_output(synchronize=True)
+print("compare two result", np.array_equal(original_out, partition_out))
+# vis = np.concatenate((original_out, partition_out), axis=1)
+# cv2.imshow('out', vis)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-    if outputs:
-        for out in outputs:
-            partition_out = out.numpy()[0]
-            top1_keras = np.argmax(partition_out)
-            total_outs.append(1)
-
-print(args.partition, args.batch_size, time.time() - now, sep=',')
 # tvm_out = top1_keras
 # top1_tvm = top1_keras
 # print("Relay top-1 id: {}, class name: {}".format(top1_tvm, synset[top1_tvm]))
-
+# """
 ################################################
-"""
-from tvm.contrib import relay_viz
-graph_attr = {"color": "red"}
-node_attr = {"color": "blue"}
-edge_attr = {"color": "black"}
-def get_node_attr(node):
-    if "add" in node.type_name and "bias_add" not in node.type_name:
-        print(node.type_name)
-        return {
-            "fillcolor": "green",
-            "style": "filled",
-            "shape": "box",
-        }
-    if "Var" in node.type_name:
-        return {"shape": "ellipse"}
-    return {"shape": "box"}
+# from tvm.contrib import relay_viz
+# graph_attr = {"color": "red"}
+# node_attr = {"color": "blue"}
+# edge_attr = {"color": "black"}
+
+# target_node_cnt = 0
+
+# def get_node_attr(node):
+#     global target_node_cnt
+#     if "nn.leaky_relu" in node.type_name:
+#         # print(node.type_name)
+#         target_node_cnt += 1
+#         return {
+#             "fillcolor": "green",
+#             "style": "filled",
+#             "shape": "box",
+#         }
+#     if "Var" in node.type_name:
+#         return {"shape": "ellipse"}
+#     return {"shape": "box"}
     
-dot_plotter = relay_viz.DotPlotter(
-    graph_attr=graph_attr,
-    node_attr=node_attr,
-    edge_attr=edge_attr,
-    get_node_attr=get_node_attr)
+# dot_plotter = relay_viz.DotPlotter(
+#     graph_attr=graph_attr,
+#     node_attr=node_attr,
+#     edge_attr=edge_attr,
+#     get_node_attr=get_node_attr)
 
-viz = relay_viz.RelayVisualizer(
-    mod,
-    relay_param=params,
-    plotter=dot_plotter,
-    parser=relay_viz.DotVizParser())
-viz.render("total")
+# viz = relay_viz.RelayVisualizer(
+#     subgraphs[1],
+#     relay_param=params,
+#     plotter=dot_plotter,
+#     parser=relay_viz.DotVizParser())
+# viz.render("subgraphs1")
+# print(target_node_cnt)
+# ann = run_opt_pass(subgraphs[0], transform.ToGraphNormalForm())
+# mod1 = tvm.IRModule.from_expr(ann)
 
-ann = run_opt_pass(subgraphs[0], transform.ToGraphNormalForm())
-mod1 = tvm.IRModule.from_expr(ann)
+# viz = relay_viz.RelayVisualizer(
+#     mod1,
+#     relay_param=params,
+#     plotter=dot_plotter,
+#     parser=relay_viz.DotVizParser())
+# viz.render("slice_0")
 
-viz = relay_viz.RelayVisualizer(
-    mod1,
-    relay_param=params,
-    plotter=dot_plotter,
-    parser=relay_viz.DotVizParser())
-viz.render("slice_0")
+# ann = run_opt_pass(subgraphs[1], transform.ToGraphNormalForm())
+# mod2 = tvm.IRModule.from_expr(ann)
 
-ann = run_opt_pass(subgraphs[1], transform.ToGraphNormalForm())
-mod2 = tvm.IRModule.from_expr(ann)
-
-viz = relay_viz.RelayVisualizer(
-    mod2,
-    relay_param=params,
-    plotter=dot_plotter,
-    parser=relay_viz.DotVizParser())
-viz.render("sliced_1")
-"""
+# viz = relay_viz.RelayVisualizer(
+#     mod2,
+#     relay_param=params,
+#     plotter=dot_plotter,
+#     parser=relay_viz.DotVizParser())
+# viz.render("sliced_1")
 
 # ################################################
 # print("load original")
